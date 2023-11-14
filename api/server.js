b99dd6964f795114fa43547a15c790852d119827
FedeFreire <bandoleir@gmail.com>
2023-11-14 08:54:42 -0500

    Practiced some middleware and routing with express

--- api/server.js
+++ api/server.js
@@ -1,16 +1,35 @@
 const express = require('express'); // importing a CommonJS module
-
+const morgan = require('morgan'); // third party middleware
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+function customMorgan(req, res, next) {
+  console.log(`it was a ${req.method} request`);
+  next();
+}
+
+function shortCircuit(req, res, next) {
+  res.json('you shall not pass!');
+}
+
+function addFriends(req, res, next) {
+  req.friends = ['mary', 'sue', 'bob'];
+  next();
+}
+
 server.use(express.json());
+server.use(morgan('dev'));
+server.use(customMorgan);
+//server.use(shortCircuit);
+server.use(addFriends);
+
 
 server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
   res.send(`
-    <h2>Hubs API</h2>
+    <h2>Hubs API ${req.friends}</h2>
     <p>Welcome to the Hubs API</p>
   `);
 });
