46b67ce047525843ba9e7906331aaa7160fa357b
FedeFreire <bandoleir@gmail.com>
2023-11-14 10:05:43 -0500

    Id check middleware implemented

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -1,89 +1,74 @@
-const express = require('express');
-
-const Hubs = require('./hubs-model.js');
-const Messages = require('../messages/messages-model.js');
+const express = require("express");
+const { checkHubId } = require("./hubs-middleware.js");
+const Hubs = require("./hubs-model.js");
+const Messages = require("../messages/messages-model.js");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   Hubs.find(req.query)
-    .then(hubs => {
+    .then((hubs) => {
       res.status(200).json(hubs);
     })
     .catch(next);
-  }); 
+});
 
-router.get('/:id', (req, res, next) => {
-  Hubs.findById(req.params.id)
-    .then(hub => {
-      if (hub) {
-        res.status(200).json(hub);
-      } else {
-        res.status(404).json({ message: 'Hub not found' });
-      }
-    })
-    .catch(next);
-  });
+router.get("/:id", checkHubId, (req, res, next) => {
+  res.json(req.hub);
+}
+);
 
-router.post('/', (req, res, next) => {
+router.post("/", (req, res, next) => {
   Hubs.add(req.body)
-    .then(hub => {
+    .then((hub) => {
       res.status(201).json(hub);
     })
     .catch(next);
-    });
+});
 
-
-router.delete('/:id', (req, res, next) => {
+router.delete("/:id", checkHubId, (req, res, next) => {
   Hubs.remove(req.params.id)
-    .then(count => {
-      if (count > 0) {
-        res.status(200).json({ message: 'The hub has been nuked' });
-      } else {
-        res.status(404).json({ message: 'The hub could not be found' });
-      }
+    .then(() => {
+      res.status(200).json({ message: "The hub has been nuked" });
     })
     .catch(next);
-  });
+});
 
-router.put('/:id', (req, res, next) => {
+router.put("/:id", checkHubId, (req, res, next) => {
   Hubs.update(req.params.id, req.body)
-    .then(hub => {
-      if (hub) {
-        res.status(200).json(hub);
-      } else {
-        res.status(404).json({ message: 'The hub could not be found' });
-      }
+    .then(() => {
+      res.status(200).json({ message: "The hub has been updated" });
     })
     .catch(next);
-  });
+});
 
-router.get('/:id/messages', (req, res, next) => {
+router.get("/:id/messages", checkHubId, (req, res, next) => {
   Hubs.findHubMessages(req.params.id)
-    .then(messages => {
+    .then((messages) => {
       res.status(200).json(messages);
     })
     .catch(next);
-    });
+});
 
-router.post('/:id/messages', (req, res, next) => {
+router.post("/:id/messages", checkHubId, (req, res, next) => {
   const messageInfo = { ...req.body, hub_id: req.params.id };
 
   Messages.add(messageInfo)
-    .then(message => {
+    .then((message) => {
       res.status(210).json(message);
     })
     .catch(next);
-  });
+});
 
-
-router.use((err, req, res, next) => { // eslint-disable-line
+router.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
-    customMessage: 'Something tragic inside the hubs router!',
+    customMessage: "Something tragic inside the hubs router!",
     message: err.message,
     stack: err.stack,
   });
-}
-);
+});
+
+
 
 module.exports = router;
